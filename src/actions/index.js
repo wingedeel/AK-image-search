a792c4dbb5b8924677dacf5fc5c42c3bb0702ae9
Anne Kotecha <anne@Annes-MBP.lan>
2017-02-04 22:04:22 +0000

    Remove comments and extra lines from code.

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,8 +5,6 @@ export const FETCH_SEARCH_RESULTS = 'FETCH_SEARCH_RESULTS';
 export const SEARCH_TERM_SELECTED = 'SEARCH_TERM_SELECTED';
 export const ITEM_SELECTED = 'ITEM_SELECTED';
 
-
-
 export function setSearchTerm(searchTerm) {
 	return {
 		type: 'SEARCH_TERM_SELECTED',
@@ -14,28 +12,19 @@ export function setSearchTerm(searchTerm) {
 	};
 }
 
-
-
-/*
-*/
 export function fetchSearchResults(searchTerm) {
 
 	// e.g. https://pixabay.com/api/?key=3447327-4862a38728b3e10a8a5e52323&q=yellow+flowers&image_type=photo
 	//var searchTerm = 'yellow+flowers';
 	var imageType = 'photo';
 	var requestUrl = ROOT_URL + '/?key=' + API_KEY + '&q=' + searchTerm + '&image_type=' + imageType;
-	console.log('requestUrl: ' + requestUrl);
-
 	const request = axios.get(requestUrl);
 
 	return {
 		type: 'FETCH_SEARCH_RESULTS',
 		payload: request
 	}
-
 }
-
-
 
 export function selectListItem(listItem) {
 	return {
--- src/containers/list.js
+++ src/containers/list.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { selectListItem } from '../actions/index';
 import { Motion, spring } from 'react-motion';
 
-
 const ListItem = ({item}) => {
 	return (
 		  <div className="result-card col-xs-6 col-sm-4">         
@@ -17,38 +16,38 @@ const ListItem = ({item}) => {
 	)
 }
 
-
 class List extends Component {
-	componentDidMount() {
-		console.log('componentDidMount')
-	}
-
-	componentWillUnmount() {
-		console.log('componentWillUnmount')
-	}
-
 	render() {
-		console.log('render')
 		return (
 			<div className="results">
 				{this.props.items.map( (item,index) => {
 					const rotateEnd = index % 2 === 0 ? 360 : -360;
 					return  (
-						<Motion key={item.id} defaultStyle={{x:0, opacity:0, scale:0}} style={{x: spring(rotateEnd,[1,1]), opacity:spring(1,[1,1]), scale:spring(1, [1,1])}}>
+						<Motion 
+							key={item.id} 
+							defaultStyle={{x:0, opacity:0, scale:0}} 
+							style={{
+								x: spring(rotateEnd,[1,1]), 
+								opacity:spring(1,[1,1]), 
+								scale:spring(1, [1,1])
+							}}>
 
-			    		{({x, opacity, scale}) => 
-			    			<div className="result-card col-xs-6 col-sm-4" key={item.id} item={item} 
-			    				style={{
-			    					border: '1px solid grey',
-			    					transform: `scale(${scale}) rotate(${x}deg)`
-			    				}}>
-				    			<div className="result-card-text-item">ID: {item.id}</div>
-				    			<div className="result-card-text-item">Tags: {item.tags}</div>
-				    			<button className="btn-view-image" onClick={() => this.props.selectListItem(item)}>View</button>
-	           					<img src={item.webformatURL} alt={item.id} />
-    						</div>
-    					}
-
+			    			{({x, opacity, scale}) => 
+				    			<div className="result-card col-xs-6 col-sm-4" key={item.id} item={item} 
+				    				style={{
+				    					border: '1px solid grey',
+				    					transform: `scale(${scale}) rotate(${x}deg)`
+				    				}}>
+					    			<div className="result-card-text-item">ID: {item.id}</div>
+					    			<div className="result-card-text-item">Tags: {item.tags}</div>
+					    			<button 
+					    				className="btn-view-image" 
+					    				onClick={() => this.props.selectListItem(item)}>
+					    			View
+					    			</button>
+		           					<img src={item.webformatURL} alt={item.id} />
+	    						</div>
+    						}
     					</Motion>
 					)
 				})}	
@@ -57,14 +56,12 @@ class List extends Component {
 	}
 }
 
-
 function mapStateToProps(state){
 	return { 
 		items: state.items.all ,
 		searchTerm: state.searchTerm
 	};
 }
-
 
 export default connect ( mapStateToProps, {selectListItem}) (List);
 
